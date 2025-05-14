50c5816dff28415c62220a18d15b7990051166a8
Paul Coppolino <hta.infinite@gmail.com>
2025-05-14 14:39:46 -0400

    Update Grocery.java

--- Grocery.java
+++ Grocery.java
@@ -1,23 +1,18 @@
 public class Grocery extends Product {
-    private final String brand;
+
     private final String department;
+    
     public Grocery(){
-        super("0","N/A",0.0,"Not available.");
-        brand = "N/A";
+        super("0","N/A",0.0,"Not available.","N/A");
         department = "N/A";
     }
 
     public Grocery(String id, String n, double p, String d, String b, String dep){
-        super(id,n,p,d);
-        this.brand = b;
+        super(id,n,p,d,b);
         this.department = dep;
     }
 
     public String getDept(){
         return department;
-    }
-
-    public String getBrand(){
-        return brand;
     }
 }
