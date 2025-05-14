b8e18cc70481098954598322d374c391241369a7
Paul Coppolino <hta.infinite@gmail.com>
2025-05-14 14:39:27 -0400

    Update Clothing.java

--- Clothing.java
+++ Clothing.java
@@ -1,17 +1,19 @@
 public class Clothing extends Product {
-    private final String brand;
+    
+    private final String size;
+
     public Clothing(){
-        super("0","N/A",0.0,"Not available.");
-        brand = "N/A";
+        super("0","N/A",0.0,"Not available.","N/A");
+        size = "N/A";
     }
 
-    public Clothing(String id, String n, double d, String desc, String brand){
-        super(id,n,d,desc);
-        this.brand = brand;
+    public Clothing(String id, String n, double d, String desc, String brand, String size){
+        super(id,n,d,desc, brand);
+        this.size = size;
     }
 
-    public String getBrand(){
-        return brand;
+    public String getSize(){
+        return size;
     }
     
 }
