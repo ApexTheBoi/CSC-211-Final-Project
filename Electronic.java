aa222610256cf760a108de32845120d85e7312d2
Paul Coppolino <hta.infinite@gmail.com>
2025-05-14 14:39:03 -0400

    Update Electronic.java

--- Electronic.java
+++ Electronic.java
@@ -1,5 +1,18 @@
 public class Electronic extends Product{
+    
+    public double voltageRequired;
+
     public Electronic(){
-        super("814281111442","Television",99.99,"A small, older television");
+        super("814281111442","Television",99.99,"A small, older television",
+        "Dell");
+    }
+
+    public Electronic(String id, String n, double p, String desc, String b, double v){
+        super(id,n,p,desc,b);
+        this.voltageRequired = v;
+    }
+
+    public double getVoltage(){
+        return voltageRequired;
     }
 }
