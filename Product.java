122e92e9c354ec498ec46b60692a3c00a5360edb
Paul Coppolino <hta.infinite@gmail.com>
2025-05-14 14:38:38 -0400

    Update Product.java

--- Product.java
+++ Product.java
@@ -3,12 +3,14 @@ public class Product { //Subclasses: Electronics | Clothing | Grocery
     private final String name;
     private final double price;
     private final String description;
+    private final String brand;
 
-    public Product(String id, String name, double price, String description){
+    public Product(String id, String name, double price, String description, String brand){
         this.ID = id;
         this.name = name;
         this.price = price;
         this.description = description;
+        this.brand = brand;
     }
 
     public String getName(){
@@ -17,6 +19,10 @@ public class Product { //Subclasses: Electronics | Clothing | Grocery
 
     public double getPrice(){
         return price;
+    }
+
+    public String getBrand(){
+        return brand;
     }
 
     @Override
